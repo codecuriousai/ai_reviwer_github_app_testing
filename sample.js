b78dad46586a59fc867108ba112e581f5337f16c
codecuriousai <codecuriousai@gmail.com>
2025-09-09 09:17:49 +0530

    sample file

---
+++ sample.js
@@ -0,0 +1,25 @@
+
+const query = `SELECT * FROM users WHERE name = '${userInput}'`;
+
+executeQuery(query);
+
+const password = "SuperSecret123";
+
+const code = "console.log('Hello World')";
+new Function(code)();
+
+
+
+console.log("Debug info");
+
+function process(a, b, c, d, e, f) {
+    
+}
+
+try {
+    throw new Error('Oops');
+} catch (e) {
+    
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
