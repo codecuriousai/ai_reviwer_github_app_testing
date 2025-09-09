c20e1416c7be8f4873eb5e2a2c54899550437e55
codecuriousai <codecuriousai@gmail.com>
2025-09-09 14:25:22 +0530

    added code

---
+++ login.js
@@ -0,0 +1,10 @@
+
+const query = 'SELECT * FROM users WHERE name = ?'; const params = [userInput]; executeQuery(query, params);
+
+executeQuery(query);
+
+const password = process.env.DB_PASSWORD;
+
+const code = "console.log('Hello World')";
+
+console.log(code);
---
+++ sample.js
@@ -0,0 +1,13 @@
+console.log("Debug info");
+
+function process(a, b, c, d, e, f) {
+
+}
+
+try {
+    throw new Error('Oops');
+} catch (e) {
+
+}
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1';
