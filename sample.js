38a0139ba8bf61133ed10cf2b0c895d18adcaaca
codecuriousai <codecuriousai@gmail.com>
2025-09-09 14:23:24 +0530

    added logic

--- sample.js
+++ sample.js
@@ -1,15 +1,3 @@
-
-const query = 'SELECT * FROM users WHERE name = ?'; const params = [userInput]; executeQuery(query, params);
-
-executeQuery(query);
-
-const password = process.env.DB_PASSWORD;
-
-const code = "console.log('Hello World')";
-console.log(code);
-
-
-
 console.log("Debug info");
 
 function process(a, b, c, d, e, f) {
