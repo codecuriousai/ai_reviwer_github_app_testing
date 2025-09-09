390dfb11ca416fb63938f34bbc3e31f7fa45078b
codecuriousai <codecuriousai@gmail.com>
2025-09-09 09:16:03 +0530

    updated code

--- sample.js
+++ sample.js
@@ -1,32 +1,12 @@
 
-// Secure SQL query using parameterized queries to prevent SQL injection.
-const query = 'SELECT * FROM users WHERE name = $1'; const params = [name]; await executeQuery(query, params);
-    const query = 'SELECT * FROM users WHERE name = $1';
-    const params = [name];
-    await executeQuery(query, params);
-    const query = 'SELECT * FROM users WHERE name = $1'; const params = [name]; await executeQuery(query, params);
-    // TODO: Use parameterized queries or ORM to prevent SQL injection.
-    const query = 'SELECT * FROM users WHERE name = $1'; const params = [name]; await executeQuery(query, params);
+const query = `SELECT * FROM users WHERE name = '${userInput}'`;
 
 executeQuery(query);
 
-const password = process.env.SECRET_PASSWORD;
+const password = "SuperSecret123";
+
 const code = "console.log('Hello World')";
-safeExecute(() => { console.log('Hello World'); });
-const safeExecute = (func) => {
-  try {
-    func();
-  } catch (error) {
-    console.error('Error executing function:', error);
-  }
-};
-safeExecute(() => { const runCode = new Function(code); return runCode(); });
-const code = "console.log('Hello World')";
-// Instead of using eval(), define a function to safely execute code
-const safeExecute = (func) => {
-    func();
-};
-safeExecute(() => { console.log('Hello World'); });
+new Function(code)();
 
 
 
@@ -36,11 +16,10 @@ function process(a, b, c, d, e, f) {
     
 }
 
-// Ensure TLS certificate validation is enabled to maintain secure connections.
-    try {
+try {
     throw new Error('Oops');
 } catch (e) {
     
 }
 
-// Remove this line to enable proper TLS certificate validation
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
